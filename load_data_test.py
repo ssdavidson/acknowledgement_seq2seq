41d7d3dd5deb10e3e2880e999c2cd11c91c2949b
ssdavidson <ssdavidson@ucdavis.edu>
2020-04-20 15:32:43 -0700

    update dataloader

--- load_data_test.py
+++ load_data_test.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 def get_data(tsv_file):
-    df = pd.read_csv(tsv_file, delimiter='\t', names=['input', 'output', 'code'], header=None)
+    df = pd.read_csv(tsv_file, delimiter='\t', names=['input', 'output', 'code'], header=None, index_col = False)
     df = df[1:]
     outputs = df.output.values
     inputs = df.input.values
