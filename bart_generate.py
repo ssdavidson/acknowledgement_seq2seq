c558806a1d38e3f3b47cf107f7a3ce2206562105
ssdavidson <ssdavidson@ucdavis.edu>
2020-04-24 10:36:55 -0700

    update generate

--- bart_generate.py
+++ bart_generate.py
@@ -28,21 +28,24 @@ def generate_summaries(
 
     for batch in tqdm(list(chunks(examples, batch_size))):
         dct = tokenizer.batch_encode_plus(batch, max_length=1024, return_tensors="pt", pad_to_max_length=True)
-        summaries = model.generate(
-            input_ids=dct["input_ids"].to(device),
-            attention_mask=dct["attention_mask"].to(device),
-            num_beams=4,
-            length_penalty=2.0,
-            max_length=max_length + 2,  # +2 from original because we start at step=1 and stop before max_length
-            min_length=min_length + 1,  # +1 from original because we start at step=1
-            no_repeat_ngram_size=3,
-            early_stopping=True,
-            decoder_start_token_id=model.config.eos_token_id,
-        )
-        dec = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summaries]
-        for hypothesis in dec:
-            fout.write(hypothesis + "\n")
-            fout.flush()
+        try:
+            summaries = model.generate(
+                input_ids=dct["input_ids"].to(device),
+                attention_mask=dct["attention_mask"].to(device),
+                num_beams=4,
+                length_penalty=2.0,
+                max_length=max_length + 2,  # +2 from original because we start at step=1 and stop before max_length
+                min_length=min_length + 1,  # +1 from original because we start at step=1
+                no_repeat_ngram_size=3,
+                early_stopping=True,
+                decoder_start_token_id=model.config.eos_token_id,
+            )
+            dec = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summaries]
+            for hypothesis in dec:
+                fout.write(hypothesis + "\n")
+                fout.flush()
+        except:
+            continue
 
 
 def run_generate():
